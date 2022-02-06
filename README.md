005f2a38281e6992ee08f34ae93dbcd96f23a6d3
Fawad Shaikh <fawad@getmeintheinbox.com>
2022-02-06 13:51:55 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# dynamic-envoy
-Experimenting with how to create vhosts and certificates with Envoy Proxy and Rust
+#Getting Started with Envoy
+To Do
+- Make a simple Rust based service that returns the hostname (replace HTTP Bin)
+- Get Envoy proxying to new Service
+- Use envoy to route two vhosts using different hostnames and point to same rust service
+- create SSL certificates for two hostnames and terminate on envoy
+- create service discovery for envoy virtual hosts
+- create secrets discovery for envoy virtual hosts TLS termination
+- create script that deploys new vhost and cert to service discovery and test that envoy can route based on new hostname
---
+++ demonstrate_front_proxy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+printf "\n\n    This script starts an envoy front proxy and exposes it on localhost:8080.\n"
+printf "    Envoy is configured to forward traffic to an httpbin instance that is only exposed inside docker.\n"
+printf "\n\n**************    Starting envoy & httpbin via docker-compose   **************\n\n"
+read -n 1 -r -s -p $'Press enter to continue...\n'
+
+docker-compose up -d --build
+
+read -n 1 -r -s -p $'Press enter to continue...\n'
+printf "\n\n**************    About to check to make sure everything is up    **************\n\n"
+read -n 1 -r -s -p $'Press enter to continue...\n'
+
+docker ps -a
+read -n 1 -r -s -p $'Press enter to continue...\n'
+printf "\n\n**************    About to call httpbin via envoy front proxy    **************\n\n"
+read -n 1 -r -s -p $'Press enter to continue...\n'
+
+curl -v --location --request GET 'http://localhost:8080/anything'
+
+read -n 1 -r -s -p $'Press enter to continue...\n'
+printf "\n\n**************    About to clean up and remove docker instances    **************\n\n"
+read -n 1 -r -s -p $'Press enter to continue...\n'
+
+docker-compose down
